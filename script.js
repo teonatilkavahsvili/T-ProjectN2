57b8c72d64e88457e56ca31cfb9bf0f0ebe99e19
teonatilkavahsvili <34924317+teonatilkavahsvili@users.noreply.github.com>
2019-12-17 22:32:51 +0400

    Add Project

--- script.js
+++ script.js
@@ -24,10 +24,59 @@
 //   });
 // });
 
-
-$(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
+$(function() {
+  $('#load-more-btn').click(loadMoreInfo);
+  $('#load-less-btn').click(loadLessInfo);
 });
+
+function loadMoreInfo() {
+  $("#load-more-progress").show();
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      $('.phone_number_details').show();
+      $('.phone_number_details.more').show();
+      $('#load-more-btn').hide();
+      $('#load-less-btn').show();
+      $('#load-more-progress').hide();
+    } else {
+      width++; 
+      $('#load-more-progress .bar').css({ width: width + '%' }); 
+      $('#load-more-progress .bar').html(width * 1  + '%');
+    }
+  }
+}
+
+function loadLessInfo() {
+  $('.phone_number_details').show();
+  $('.phone_number_details.more').hide();
+  $('#load-more-btn').show();
+  $('#load-less-btn').hide();
+}
+
+// function move(){
+//   document.getElementById("more").style.display = "inline";
+// }
+
+function myFunction() {
+   var element = document.getElementById("LoadClass");
+   element.classList.add("loader");
+}
+
+$(document).ready(function(){
+
+       setTimeout(function(){
+          $('body').addClass('loaded');
+          $('h1').css('color','#222222');
+        }, 3000);
+
+});
+// $ ('body').addClass('full_information');
+// $(document).ready(function() {
+//   $('[data-toggle="tooltip"]').tooltip();
+// });
 
 
 function myFunction() {
